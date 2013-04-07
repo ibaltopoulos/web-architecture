91017162e6427f929ad4ded4ab696f537af47ea0
ibaltopoulos <ioannis.baltopoulos@gmail.com>
2013-04-07 17:45:06 +0100

    Clean up the project directories and add a simple model.

---
+++ DeskManager.Core/Desk.cs
@@ -0,0 +1,31 @@
+﻿namespace DeskManager.Core
+{
+    public class Desk
+    {
+        public string Id { get; set; }
+        public string Number { get; set; }
+        public Person Occupier { get; set; }
+
+        public bool IsEmpty()
+        {
+            return !IsOccupied();
+        }
+
+        public bool IsOccupied()
+        {
+            return Occupier != null;
+        }
+    }
+
+    public enum DeskStatus
+    {
+        Empty,
+        Occupied
+    }
+
+    public class Person
+    {
+        public string Id { get; set; }
+    }
+
+}
